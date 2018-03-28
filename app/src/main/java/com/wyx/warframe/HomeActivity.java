07f77c5f3cf3f9f8930d57bb905492a75b71100c
wyx <wengyx@jkinvest.cn>
2018-03-28 16:27:35 +0800

    添加欢迎页

---
+++ app/src/main/java/com/wyx/warframe/HomeActivity.java
@@ -0,0 +1,17 @@
+package com.wyx.warframe;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+/**
+ * Created by wyx on 2018/3/28.
+ */
+
+public class HomeActivity extends Activity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
--- app/src/main/java/com/wyx/warframe/MainActivity.java
+++ app/src/main/java/com/wyx/warframe/MainActivity.java
@@ -1,14 +1,30 @@
 package com.wyx.warframe;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 
-public class MainActivity extends AppCompatActivity {
+/**
+ * 欢迎页面
+ */
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        RetrofitFactory.createService(HttpService.class).
+        //展示3秒后跳转主页面
+        handler.sendEmptyMessageDelayed(0,3000);
     }
+    private Handler handler = new Handler(new Handler.Callback() {
+        @Override
+        public boolean handleMessage(Message msg) {
+            Intent intent = new Intent(getBaseContext(),HomeActivity.class);
+            startActivity(intent);
+            finish();
+            return false;
+        }
+    });
 }
